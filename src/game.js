753c16bdb80dce4a659fcd1416180ea92bc6d25a
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-07-31 22:35:32 -0700

    WIP functionality for adding ships to board

--- src/game.js
+++ src/game.js
@@ -1,5 +1,10 @@
 import './style.css';
 import Player from './player';
+import './images/Alpha.png'
+import './images/Beta.png'
+import './images/Gamma.png'
+import './images/Delta.png'
+import './images/Epsilon.png'
 
 const input = document.querySelector('.input');
 const button = document.querySelector('.button');
@@ -7,10 +12,24 @@ const playerNameElement = document.querySelector('.player-name');
 const reset = document.querySelector('.reset');
 const shipContainer = document.querySelector('.ship-container');
 const board = document.querySelector('.gameboard');
+const axis = document.querySelector('.axis');
 
+let vertical = false;
 reset.addEventListener('click', () => {
     player1.myBoard.resetBoard();
-    Array.from(document.querySelectorAll('.column')).forEach((elem) => elem.style.backgroundColor = '');
+    board.removeChild(alpha);
+    board.removeChild(beta);
+    board.removeChild(gamma);
+    board.removeChild(delta);
+    board.removeChild(epsilon);
+    
+    shipContainer.appendChild(alpha);
+    shipContainer.appendChild(beta);
+    shipContainer.appendChild(gamma);
+    shipContainer.appendChild(delta);
+    shipContainer.appendChild(epsilon);
+
+    Array.from(document.querySelectorAll('.cell')).forEach((elem) => elem.style.backgroundColor = '');
     console.log(player1.myBoard)
 });
 
@@ -36,12 +55,13 @@ let currentShip;
 for (let i = 0; i < ships.length; i++) {
     ships[i].addEventListener('click', () => {
         currentShip = ships[i];
-        alpha.style.filter = beta.style.filter = gamma.style.filter = delta.style.filter = epsilon.style.filter = 'invert(1)'
-        currentShip.style.filter = 'none';
+        alpha.style.border = beta.style.border = gamma.style.border = delta.style.border = epsilon.style.border = '';
+            currentShip.style.border = '2px solid black';
     });
 };
 
-const cells = Array.from(document.querySelectorAll('.column'));
+const cells = Array.from(document.querySelectorAll('.cell'));
+const shipCells = [];
 
 for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', () => {
@@ -50,27 +70,77 @@ for (let i = 0; i < cells.length; i++) {
         let endX;
         let endY = startY;
         let ship = currentShip.classList[1]
+        let len
         switch (ship) {
-            case 'alpha': endX = startX + 1;break;
-            case 'beta': endX = startX + 2;break;
-            case 'gamma': endX = startX + 2;break;
-            case 'delta': endX = startX + 3;break;
-            case 'epsilon': endX = startX + 4;break;
+            case 'alpha': endX = startX + 1; len = 2; break;
+            case 'beta': endX = startX + 2; len = 3; break;
+            case 'gamma': endX = startX + 2; len = 3; break;
+            case 'delta': endX = startX + 3; len = 4; break;
+            case 'epsilon': endX = startX + 4; len = 5; break;
         };
         let start = [startX, startY];
         let end = [endX, endY];
         player1.myBoard.setShip(player1[ship], start, end);
-        let length = Math.abs(endX + 1 - startX);
-        shipContainer.removeChild(currentShip);
-        board.appendChild(currentShip);
-        console.log(startX);
-        console.log(startY);
+        if (currentShip.parentElement !== shipContainer) {
+            let curr = currentShip.parentElement.nextElementSibling;
+            for (let h = 0; h < len - 1; h++) {
+                curr.removeChild(curr.children[0]);
+                curr = curr.nextElementSibling;
+            };
+        };
+        currentShip.parentElement.removeChild(currentShip);
+        if (shipContainer.childElementCount === 0 && shipContainer.parentElement === document.body) {
+            document.body.removeChild(shipContainer);
+        };
+        cells[i].appendChild(currentShip);
+        if (vertical) {
+            currentShip.style.transform = 'rotate(90deg)';
+            currentShip.style.top = `${(44 * (0.5 * (len - 1)))}px`;
+            currentShip.style.left = `-${(44 * (0.5 * (len - 1))) - (len - 2) * 6}px`;
+            if (currentShip.classList[1] === 'beta') {
+                currentShip.style.left = `-${(44 * (0.5 * (len - 1))) - (len - 2) * 18}px`;
+            }
+            for (let k = 0; k < endX - startX + 1; k++) {
+                cells[i + (k * 10)].style.backgroundColor = 'green';
+                if (k > 0) {
+                    cells[i + (k * 10)].appendChild(document.createElement('div'));
+                }
+            };
+        } else {
+            for (let k = 0; k < endX - startX + 1; k++) {
+                cells[i + k].style.backgroundColor = 'green';
+                if (k > 0) {
+                    cells[i + k].appendChild(document.createElement('div'));
+                }
+            };
+        };
         currentShip.style.margin = '0';
         currentShip.style.position = 'absolute';
-        currentShip.style.top = `${(startY + 1) * 44 + 2}px`;
-        currentShip.style.left = `${(startX + 1) * 46 + 4}px`;
-        for (let j = 0; j < length; j++) {
-            cells[i + j].style.backgroundColor = 'blue';
+        for (let j = 0; j < cells.length; j++) {
+            if (cells[j].style.backgroundColor === 'green' && cells[j].childElementCount === 0) {
+                cells[j].style.backgroundColor = '';
+            };
         };
+  
     });
 };
+
+window.addEventListener('click', e => {
+    if (!e.target.classList.contains('ship') && currentShip) {
+        currentShip.style.border = 'none';
+    };
+});
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Shift' && !vertical) {
+        vertical = true;
+        axis.textContent = 'VERTICAL';
+    };
+});
+
+window.addEventListener('keyup', (e) => {
+    if (e.key === 'Shift' && vertical) {
+        vertical = false
+        axis.textContent = 'HORIZONTAL';
+    };
+});
