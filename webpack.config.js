ea3c4a1b8451e7b0538eb15dd175847a264e45ac
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-08-21 11:46:39 -0700

    Add gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         template: './src/index.html'
     }),
   ],
-  mode: 'development',
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
